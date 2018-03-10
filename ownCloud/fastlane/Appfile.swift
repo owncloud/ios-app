bdd47e0269611cd5e18b6e24c03099249cba100f
Christian Scherm <cscherm@owncloud.com>
2018-03-10 01:23:05 +0100

    Fastlane basic caniguration for taking automatic screenshots
    Fastlane basic caniguration for taking automatic screenshots

---
+++ ownCloud/fastlane/Appfile.swift
@@ -0,0 +1,7 @@
+var appIdentifier: String { return "com.owncloud.newAppTest" } // The bundle identifier of your app
+var appleID: String { return "[[APPLE_ID]]" } // Your Apple email address
+
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
