f1dbbabfd2b3ef994a3630ae81a219646d6d2a0d
Pablo Carrascal <pcarrascal@solidgeargroup.com>
2018-04-05 23:59:08 +0200

    - Introduced alpha argument into custom UIColor init methods

--- ownCloud/UIKit Extensions/UIColor+Extension.swift
+++ ownCloud/UIKit Extensions/UIColor+Extension.swift
@@ -10,15 +10,15 @@ import UIKit
 
 extension UIColor {
 
-    convenience init(red: Int, green: Int, blue: Int) {
+    convenience init(red: Int, green: Int, blue: Int, alpha: Float = 1.0) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
 
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: CGFloat(alpha))
     }
 
-    convenience init(hex rgbHex: Int) {
-        self.init(red: (rgbHex >> 16) & 0xFF, green: (rgbHex >> 8) & 0xFF, blue: (rgbHex & 0xFF))
+    convenience init(hex rgbHex: Int, alpha: Float = 1.0) {
+        self.init(red: (rgbHex >> 16) & 0xFF, green: (rgbHex >> 8) & 0xFF, blue: (rgbHex & 0xFF), alpha: alpha)
     }
 }
