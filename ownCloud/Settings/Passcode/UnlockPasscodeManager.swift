b1a1cc2b2145e11114729f8a8f8e0c1ad1c3f43e
Javier Gonzalez <javiergonzper@gmail.com>
2018-05-10 14:05:18 +0200

    Working on add the biometrical unlock

--- ownCloud/Settings/Passcode/UnlockPasscodeManager.swift
+++ ownCloud/Settings/Passcode/UnlockPasscodeManager.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import ownCloudSDK
+import LocalAuthentication
 
 let DateHomeButtonPressedKey = "date-home-button-pressed"
 
@@ -15,6 +16,8 @@ class UnlockPasscodeManager: NSObject {
 
     private var passcodeViewController: PasscodeViewController?
     private var userDefaults: UserDefaults?
+
+    var hanlder:PasscodeHandler?
 
     static var sharedUnlockPasscodeManager : UnlockPasscodeManager = {
         let sharedInstance = UnlockPasscodeManager()
@@ -30,21 +33,22 @@ class UnlockPasscodeManager: NSObject {
     // MARK: - Unlock device
 
     func showPasscodeIfNeeded(viewController: UIViewController, hiddenOverlay:Bool) {
-
         if isPasscodeActivated() {
 
             storeDateHomeButtonPressed()
 
             if self.passcodeViewController == nil {
-                self.passcodeViewController = PasscodeViewController(mode: PasscodeInterfaceMode.unlockPasscode, hiddenOverlay:false, handler: {
+
+                hanlder = {
                     DispatchQueue.main.async {
                         self.userDefaults?.removeObject(forKey: DateHomeButtonPressedKey)
-                        viewController.dismiss(animated: true, completion: nil)
+                        self.passcodeViewController?.dismiss(animated: true, completion: nil)
                         self.passcodeViewController = nil
                     }
-                })
+                }
 
-                viewController.present(self.passcodeViewController!, animated: false, completion: nil)
+                self.passcodeViewController = PasscodeViewController(mode: PasscodeInterfaceMode.unlockPasscode, hiddenOverlay:false, handler: hanlder)
+                viewController.present(self.passcodeViewController!, animated: true, completion: nil)
             } else {
                 self.passcodeViewController?.showOverlay()
             }
@@ -69,6 +73,7 @@ class UnlockPasscodeManager: NSObject {
             }
         } else {
             hideOverlay()
+            self.authenticateUser(hanlder: self.hanlder!)
         }
     }
 
@@ -113,6 +118,28 @@ class UnlockPasscodeManager: NSObject {
         if OCAppIdentity.shared().keychain.readDataFromKeychainItem(forAccount: passcodeKeychainAccount, path: passcodeKeychainPath) != nil,
             self.userDefaults?.data(forKey: DateHomeButtonPressedKey) == nil {
             self.userDefaults?.set(NSKeyedArchiver.archivedData(withRootObject: Date()), forKey: DateHomeButtonPressedKey)
+        }
+    }
+
+    // MARK: - Biometrical
+
+    public func authenticateUser(hanlder: @escaping PasscodeHandler) {
+        // Get the local authentication context.
+        let context = LAContext()
+
+        // Set the reason string that will appear on the authentication alert.
+        let reasonString = "Unlock".localized
+
+        // Declare a NSError variable.
+        var error: NSError?
+
+        // Check if the device can evaluate the policy.
+        if context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString) { (success, error) in
+                if success {
+                    hanlder()
+                }
+            }
         }
     }
 }
