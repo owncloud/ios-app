6925cb5a4effd383487a5cac27381995f7337153
Javier Gonzalez <javiergonzper@gmail.com>
2018-05-15 17:43:31 +0200

    Added a UIWindow extension to get the last UIViewController visible in order to show the PasscodeViewController from it

--- ownCloud/AppDelegate.swift
+++ ownCloud/AppDelegate.swift
@@ -39,7 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.addSubview((navigationController?.view)!)
         window?.makeKeyAndVisible()
 
-        UnlockPasscodeManager.sharedUnlockPasscodeManager.showPasscodeIfNeeded(viewController: (window?.rootViewController)!, hiddenOverlay: false)
+        UnlockPasscodeManager.sharedUnlockPasscodeManager.showPasscodeIfNeeded(viewController: (window?.visibleViewController)!, hiddenOverlay: false)
 
 		return true
 	}
@@ -47,7 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        UnlockPasscodeManager.sharedUnlockPasscodeManager.showPasscodeIfNeeded(viewController: (window?.rootViewController)!, hiddenOverlay: false)
+        UnlockPasscodeManager.sharedUnlockPasscodeManager.showPasscodeIfNeeded(viewController: (window?.visibleViewController)!, hiddenOverlay: false)
 	}
 
 	func applicationDidEnterBackground(_ application: UIApplication) {
