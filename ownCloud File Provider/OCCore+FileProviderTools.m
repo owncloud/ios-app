f072bec58dd850f1395e7ca64a3df5051171ec1d
Felix Schwarz <felix.schwarz@iospirit.com>
2018-09-19 14:31:31 +0200

    - FileProvider changes 	- Modernized to use new OCSyncExec macros for more clarity, finding and fixing timing bugs along the way 	- Stopped removing files in -stopProvidingItemAtURL 	- Added support for importing documents (=> uploading) - Updated to newer SDK

--- ownCloud File Provider/OCCore+FileProviderTools.m
+++ ownCloud File Provider/OCCore+FileProviderTools.m
@@ -24,52 +24,23 @@
 {
 	__block OCItem *item = nil;
 
-	dispatch_group_t waitForRetrievalGroup = dispatch_group_create();
+	OCSyncExec(databaseRetrieval, {
+		[self.vault.database retrieveCacheItemForFileID:fileID completionHandler:^(OCDatabase *db, NSError *error, OCSyncAnchor syncAnchor, OCItem *itemFromDatabase) {
+			item = itemFromDatabase;
 
-	dispatch_group_enter(waitForRetrievalGroup);
+			if (outSyncAnchor != NULL)
+			{
+				*outSyncAnchor = syncAnchor;
+			}
 
-	[self.vault.database retrieveCacheItemForFileID:fileID completionHandler:^(OCDatabase *db, NSError *error, OCSyncAnchor syncAnchor, OCItem *itemFromDatabase) {
-		item = itemFromDatabase;
+			if (outError != NULL)
+			{
+				*outError = error;
+			}
 
-		if (outSyncAnchor != NULL)
-		{
-			*outSyncAnchor = syncAnchor;
-		}
-
-		if (outError != NULL)
-		{
-			*outError = error;
-		}
-
-		dispatch_group_leave(waitForRetrievalGroup);
-	}];
-
-	dispatch_group_wait(waitForRetrievalGroup, DISPATCH_TIME_FOREVER);
-
-	/*
-	dispatch_group_t waitForDatabaseGroup = dispatch_group_create();
-
-	dispatch_group_enter(waitForDatabaseGroup);
-
-
-	[self retrieveItemFromDatabaseForFileID:fileID completionHandler:^(NSError *error, OCSyncAnchor syncAnchor, OCItem *itemFromDatabase) {
-		item = itemFromDatabase;
-
-		if (outSyncAnchor != NULL)
-		{
-			*outSyncAnchor = syncAnchor;
-		}
-
-		if (outError != NULL)
-		{
-			*outError = error;
-		}
-
-		dispatch_group_leave(waitForDatabaseGroup);
-	}];
-
-	dispatch_group_wait(waitForDatabaseGroup, DISPATCH_TIME_FOREVER);
-	*/
+			OCSyncExecDone(databaseRetrieval);
+		}];
+	});
 
 	return (item);
 }
