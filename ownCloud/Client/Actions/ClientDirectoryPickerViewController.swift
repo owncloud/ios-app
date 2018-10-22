5c732545a393ee03cd6fd03efd78fa7774179c6f
Pablo Carrascal <simplepablete@gmail.com>
2018-10-22 12:15:51 +0200

    Move files/ folder between folders. (#110)
    * - First implementation of drag & drop with multiple files support (WIP).
    - First implementation of the move feature through a new screen (WIP).
    
    * - Coded the delegate methods for drag & drop support.
    - Removed unneded code for drag & drop in ClientItemCell.
    
    * - Coded hidden footer in ClientDirectoryPickerViewController to avoid extra cell separators
    
    * - Localize 'Move Here' message
    
    * - Move feature now it's part of the More view.
    - Duplicate feature it's now part of the More view
    
    * - Ban drop items over items that are not folders
    
    * - Bring to the front of the view the "move here" button to fix when there is no elements in a folder.
    
    * - Now if you drop one item inside another that is a file, it takes the file's folder path and will move the file to it.
    
    * - Removed duplicate feature from this PR.
    
    * - Removed redundant Optional initializers.
    
    * - SDK Updated
    
    * - Removed unused MobileCoreServices from OCItem+Extension.
    - Removed unused 'canHandle...' function from OCItem+Extension.
    - Fix Move here typo.
    
    * - Coded new filter to only show folders in the directory picker.
    - Made the query inside the directory picker so now the init argument is
    the path.
    
    * - Made the bottom inset of the directory picker table view to match the
    heigth of the picker bottom button.
    - Made editable the name of the directory picker bottom button.
    
    * - Replaced the bottom move button with a UIToolbar and a UIBarButton.

---
+++ ownCloud/Client/Actions/ClientDirectoryPickerViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ClientDirectoryPickerViewController.swift
+//  ownCloud
+//
+//  Created by Pablo Carrascal on 22/08/2018.
+//  Copyright © 2018 ownCloud GmbH. All rights reserved.
+//
+
+import UIKit
+import ownCloudSDK
+
+class ClientDirectoryPickerViewController: ClientQueryViewController {
+
+	private let SELECT_BUTTON_HEIGHT: CGFloat = 44.0
+
+	// MARK: - Query directory filter
+	private static let DIRECTORY_FILTER_IDENTIFIER: String = "directory-filter"
+	private static var directoryFilterHandler: OCQueryFilterHandler = { (_, _, item) -> Bool in
+		if let item = item {
+			if item.type == .collection {return true}
+		}
+		return false
+	}
+	private static var directoryFilter: OCQueryFilter {
+		return OCQueryFilter(handler: ClientDirectoryPickerViewController.directoryFilterHandler)
+	}
+
+	// MARK: - Instance Properties
+	private var selectButton: UIBarButtonItem!
+	private var selectButtonTitle: String
+	private var cancelBarButton: UIBarButtonItem!
+	private var completion: (OCItem) -> Void
+
+	// MARK: - Init & deinit
+	init(core inCore: OCCore, path: String, selectButtonTitle: String = "Move here".localized, completion: @escaping (OCItem) -> Void) {
+		self.selectButtonTitle = selectButtonTitle
+		self.completion = completion
+
+		super.init(core: inCore, query: OCQuery(forPath: path)!)
+
+		self.query?.addFilter(ClientDirectoryPickerViewController.directoryFilter, withIdentifier: ClientDirectoryPickerViewController.DIRECTORY_FILTER_IDENTIFIER)
+
+		Theme.shared.register(client: self)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	deinit {
+		Theme.shared.unregister(client: self)
+	}
+
+	// MARK: - ViewController lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Select button creation
+		selectButton = UIBarButtonItem(title: selectButtonTitle, style: .plain, target: self, action: #selector(selectButtonPressed))
+		selectButton.title = selectButtonTitle
+
+		// Cancel button creation
+		cancelBarButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelBarButtonPressed))
+		navigationItem.rightBarButtonItem = cancelBarButton
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(true)
+
+		if let navController = self.navigationController {
+			navController.isToolbarHidden = false
+			let flexibleSpaceBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+			self.setToolbarItems([flexibleSpaceBarButton, selectButton, flexibleSpaceBarButton], animated: false)
+		}
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		guard let items = items else {
+			return
+		}
+
+		let item = items[indexPath.row]
+
+		guard item.type == OCItemType.collection else {
+			return
+		}
+
+		self.navigationController?.pushViewController(ClientDirectoryPickerViewController(core: core!, path: item.path, completion: completion), animated: true)
+	}
+
+	override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+		return nil
+	}
+
+	// MARK: - Actions
+	@objc private func cancelBarButtonPressed() {
+		self.dismiss(animated: true)
+	}
+
+	@objc private func selectButtonPressed() {
+		guard let query = query else {
+			return
+		}
+
+		self.dismiss(animated: true, completion: {
+			self.completion(query.rootItem)
+		})
+	}
+}
