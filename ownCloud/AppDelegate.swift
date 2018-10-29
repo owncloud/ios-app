438af6f84f8d737ede8bc9cd1c89b80110918f39
Felix Schwarz <felix.schwarz@iospirit.com>
2018-10-29 11:50:16 +0100

    - Cleanup

--- ownCloud/AppDelegate.swift
+++ ownCloud/AppDelegate.swift
@@ -28,7 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		var navigationController: UINavigationController?
-		var activeThemeCollection : ThemeCollection?
 
 		window = UIWindow(frame: UIScreen.main.bounds)
 
