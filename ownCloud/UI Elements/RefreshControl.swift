678c3b7dbb01eb9713cb9e9a9ca09617fc15d5a7
Pablo Carrascal <simplepablete@gmail.com>
2018-11-29 10:02:13 +0100

    - Pasted the License info.

--- ownCloud/UI Elements/RefreshControl.swift
+++ ownCloud/UI Elements/RefreshControl.swift
@@ -6,8 +6,21 @@
 //  Copyright © 2018 ownCloud GmbH. All rights reserved.
 //
 
+/*
+* Copyright (C) 2018, ownCloud GmbH.
+*
+* This code is covered by the GNU Public License Version 3.
+*
+* For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+* You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+*
+*/
+
 import UIKit
 
+
+/// This class is base on the solution proposed here https://stackoverflow.com/a/50670500
+// It's the only way I found that fixed the white line problem.
 class RefreshControl: UIRefreshControl {
 
 	override var isHidden: Bool {
