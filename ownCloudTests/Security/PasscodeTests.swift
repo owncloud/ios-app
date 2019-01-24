e45068f7df1028f83cc63624d8fb823417148b22
Felix Schwarz <felix.schwarz@iospirit.com>
2019-01-24 16:07:00 +0100

    - Update SDK to latest

--- ownCloudTests/Security/PasscodeTests.swift
+++ ownCloudTests/Security/PasscodeTests.swift
@@ -17,7 +17,7 @@ class PasscodeTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+	OCBookmarkManager.deleteAllBookmarks(waitForServerlistRefresh: true)
     }
 
     override func tearDown() {
