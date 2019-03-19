cffc3befca2b676a4ae9781ea25c0cf137231a1a
Javier Gonzalez <javiergonzper@gmail.com>
2019-03-19 18:31:55 +0100

    - Created a new target for UITesting to take the screenshots - Created a new scheme for UITesting to take the screenthots - Added an initial config to take the screenshots with fastlane - Added the SnapshotHelper.swift with the code of fastlane to create the screenshots - Added a UI Test to take a first screenshot

---
+++ ownCloudScreenshotsTests/ownCloudScreenshotsTests.swift
@@ -0,0 +1,47 @@
+//
+//  ownCloudScreenshotsTests.swift
+//  ownCloudScreenshotsTests
+//
+//  Created by Javier Gonzalez on 19/03/2019.
+//  Copyright © 2019 ownCloud GmbH. All rights reserved.
+//
+
+/*
+* Copyright (C) 2018, ownCloud GmbH.
+*
+* This code is covered by the GNU Public License Version 3.
+*
+* For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+* You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+*
+*/
+
+import XCTest
+import EarlGrey
+import ownCloudSDK
+import LocalAuthentication
+
+@testable import ownCloud
+
+class ownCloudScreenshotsTests: XCTestCase {
+
+    override func setUp() {
+		super.setUp()
+        continueAfterFailure = false
+    }
+
+    override func tearDown() {
+		super.tearDown()
+    }
+
+	func testScreenshot01Login() {
+
+		let app = XCUIApplication()
+		app.launchEnvironment = ["oc:app.show-beta-warning": "false"]
+		setupSnapshot(app)
+		app.launch()
+
+		snapshot("01LoginView")
+		
+	}
+}
