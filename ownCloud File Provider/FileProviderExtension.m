2ce2d51cbb56da477001db5cbad02e90b2991926
Matthias Hühne <github@hosy.de>
2019-06-21 14:35:22 +0200

    removed debug messages for file provider

--- ownCloud File Provider/FileProviderExtension.m
+++ ownCloud File Provider/FileProviderExtension.m
@@ -859,11 +859,8 @@
 	NSUserDefaults *userDefaults = [[OCAppIdentity sharedAppIdentity] userDefaults];
 	if ([userDefaults boolForKey:@"applock-lock-enabled"])
 	{
-		NSLog(@"--> applock-lock-enabled");
 		NSData *lockedDateData = [[[OCAppIdentity sharedAppIdentity] keychain] readDataFromKeychainItemForAccount:@"app.passcode" path:@"lockedDate"];
 		NSData *unlockData = [[[OCAppIdentity sharedAppIdentity] keychain] readDataFromKeychainItemForAccount:@"app.passcode" path:@"unlocked"];
-
-		NSLog(@"--> data: %ld %ld %@", [lockedDateData length], [lockedDateData length], [userDefaults objectForKey:@"applock-lock-delay"]);
 
 		if (lockedDateData != nil && unlockData != nil && [userDefaults objectForKey:@"applock-lock-delay"] != nil)
 		{
@@ -871,7 +868,6 @@
 			NSDate *lockDate = [NSKeyedUnarchiver unarchiveObjectWithData:lockedDateData];
 			BOOL unlocked = [[NSKeyedUnarchiver unarchiveObjectWithData:unlockData] boolValue];
 
-			NSLog(@"--> %ld %@ %d", lockDelay, lockDate, unlocked);
 			if ( !unlocked || (unlocked == true && [[lockDate dateByAddingTimeInterval:lockDelay] compare:[NSDate date]] == NSOrderedAscending))
 			{
 				*error = [NSError errorWithDomain:NSFileProviderErrorDomain code:NSFileProviderErrorNotAuthenticated userInfo:nil];
@@ -879,13 +875,11 @@
 				return (nil);
 			}
 		} else if (unlockData != nil && ![[NSKeyedUnarchiver unarchiveObjectWithData:unlockData] boolValue]) {
-			NSLog(@"--> else");
 			*error = [NSError errorWithDomain:NSFileProviderErrorDomain code:NSFileProviderErrorNotAuthenticated userInfo:nil];
 
 			return (nil);
 		}
 	}
-	NSLog(@"--> load content");
 
 	if (self.domain.identifier == nil)
 	{
