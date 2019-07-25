2a6e2d8c22e9d5b5ff9ea4c0ad59379616e57465
Michael Neuwert <michael.neuwert@googlemail.com>
2019-07-25 11:21:44 +0200

    Added dummy implementation of media upload task

---
+++ ownCloud/Client/Tasks/InstantMediaUploadTaskExtension.swift
@@ -0,0 +1,34 @@
+//
+//  InstantMediaUploadTaskExtension.swift
+//  ownCloud
+//
+//  Created by Michael Neuwert on 24.07.2019.
+//  Copyright © 2019 ownCloud GmbH. All rights reserved.
+//
+
+/*
+* Copyright (C) 2018, ownCloud GmbH.
+*
+* This code is covered by the GNU Public License Version 3.
+*
+* For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+* You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+*
+*/
+
+import Foundation
+import ownCloudSDK
+
+class InstantMediaUploadTaskExtension : ScheduledTaskAction, OCCoreDelegate {
+	override class var identifier : OCExtensionIdentifier? { return OCExtensionIdentifier("com.owncloud.action.instant_media_upload") }
+	override class var locations : [OCExtensionLocationIdentifier]? { return [.appBackgroundFetch] }
+	override class var features : [String : Any]? { return [ FeatureKeys.runOnWifi : true] }
+
+	override func run(background:Bool) {
+		
+	}
+
+	func core(_ core: OCCore, handleError error: Error?, issue: OCIssue?) {
+		// TODO
+	}
+}
