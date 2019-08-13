2c51b97ef6ac432517418c42e63e28f7a3862ed8
Michael Neuwert <michael.neuwert@googlemail.com>
2019-08-13 21:59:22 +0300

    Fixed a trailing whitespace warning

--- ownCloud/Client/Sharing/PublicLinkTableViewController.swift
+++ ownCloud/Client/Sharing/PublicLinkTableViewController.swift
@@ -258,7 +258,6 @@ class PublicLinkTableViewController: SharingTableViewController {
 	@objc func addPublicLink() {
 		if let path = item.path, let core = core {
 			var permissions : OCSharePermissionsMask = [.create, .read]
-
 			let share = OCShare(publicLinkToPath: path, linkName: defaultLinkName(), permissions: permissions, password: nil, expiration: nil)
 			let editPublicLinkViewController = PublicLinkEditTableViewController(share: share, core: core, item: self.item, defaultLinkName: defaultLinkName())
 			editPublicLinkViewController.createLink = true
