c7494d749477dd1d4e982ecb057822998dd01746
Michael Neuwert <michael.neuwert@googlemail.com>
2019-08-31 15:40:52 +0200

    Fixed double-tap to zoom and added single tap to hide bars

--- ownCloud/Client/Viewer/PreviewViewController.swift
+++ ownCloud/Client/Viewer/PreviewViewController.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2019 ownCloud GmbH. All rights reserved.
 //
 
+/*
+* Copyright (C) 2018, ownCloud GmbH.
+*
+* This code is covered by the GNU Public License Version 3.
+*
+* For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+* You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+*
+*/
+
 import UIKit
 import ownCloudSDK
 import QuickLook
@@ -45,6 +55,9 @@ class PreviewViewController : DisplayViewController, QLPreviewControllerDataSour
 			qlPreviewController?.reloadData()
 
 			self.tapToHideBarsGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapToHideBars))
+			self.tapToHideBarsGestureRecognizer.delegate = self
+			self.tapToHideBarsGestureRecognizer.delaysTouchesBegan = true
+			self.qlPreviewController?.view.gestureRecognizers?.forEach({ $0.delegate = self })
 			self.qlPreviewController?.view?.addGestureRecognizer(self.tapToHideBarsGestureRecognizer)
 
 			completion(true)
@@ -81,6 +94,20 @@ class PreviewViewController : DisplayViewController, QLPreviewControllerDataSour
 	func previewController(_ controller: QLPreviewController, editingModeFor previewItem: QLPreviewItem) -> QLPreviewItemEditingMode {
 		// Return .updateContents so QLPreviewController takes care of updating the contents of the provided QLPreviewItems whenever users save changes.
 		return .updateContents
+	}
+}
+
+// MARK: - Gesture recognizer delegete.
+extension PreviewViewController: UIGestureRecognizerDelegate {
+	func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+						   shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+		// Don't recognize a single tap until a double-tap fails.
+		if let otherTapGestureRecognizer = otherGestureRecognizer as? UITapGestureRecognizer {
+			if gestureRecognizer == self.tapToHideBarsGestureRecognizer && otherTapGestureRecognizer.numberOfTapsRequired == 2 {
+				return true
+			}
+		}
+		return false
 	}
 }
 
