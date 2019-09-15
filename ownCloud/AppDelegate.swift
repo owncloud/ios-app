6ea61140ce4a885b40915c9be7ce6ccb87613320
Michael Neuwert <mneuwert@mneuwert.fritz.box>
2019-09-15 16:48:13 +0200

    First commit on catalyst branch
    - App runs, window is shown
    - When adding new account it hangs

--- ownCloud/AppDelegate.swift
+++ ownCloud/AppDelegate.swift
@@ -134,14 +134,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}
 
     // MARK: UISceneSession Lifecycle
-@available(iOS 13.0, *)
+@available(iOS 13.0, macOS 10.15, *)
 func application(_ application: UIApplication,
 				 configurationForConnecting connectingSceneSession: UISceneSession,
 				 options: UIScene.ConnectionOptions) -> UISceneConfiguration {
 	return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 }
 
-    @available(iOS 13.0, *)
+    @available(iOS 13.0, macOS 10.15, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         print("AppDelegate didDiscardSceneSessions")
     }
