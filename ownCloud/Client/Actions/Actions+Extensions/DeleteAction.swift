a89cd6866542b1b83fa44e7b3874b8c3aaeb5216
Michael Neuwert <michael.neuwert@googlemail.com>
2019-10-09 09:53:38 +0200

    Fixed mission icons for ‘moreFolder’ location (#523)

--- ownCloud/Client/Actions/Actions+Extensions/DeleteAction.swift
+++ ownCloud/Client/Actions/Actions+Extensions/DeleteAction.swift
@@ -100,7 +100,7 @@ class DeleteAction : Action {
 	}
 
 	override class func iconForLocation(_ location: OCExtensionLocationIdentifier) -> UIImage? {
-		if location == .moreItem {
+		if location == .moreItem || location == .moreFolder {
 			return UIImage(named: "trash")
 		}
 
