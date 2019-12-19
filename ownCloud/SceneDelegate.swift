74171f82757475b452c967633924a0028b1b454c
Matthias Hühne <github@hosy.de>
2019-12-19 10:01:47 +0100

    using ThemeWindow in SceneDelegate

--- ownCloud/SceneDelegate.swift
+++ ownCloud/SceneDelegate.swift
@@ -21,12 +21,12 @@ import ownCloudSDK
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-	var window: UIWindow?
+	var window: ThemeWindow?
 
 	// UIWindowScene delegate
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		if let windowScene = scene as? UIWindowScene {
-			window = UIWindow(windowScene: windowScene)
+			window = ThemeWindow(windowScene: windowScene)
 			let serverListTableViewController = ServerListTableViewController(style: UITableView.Style.plain)
 			serverListTableViewController.restorationIdentifier = "ServerListTableViewController"
 			let navigationController = ThemeNavigationController(rootViewController: serverListTableViewController)
@@ -44,7 +44,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		return scene.userActivity
 	}
 
-	@discardableResult func configure(window: UIWindow?, with activity: NSUserActivity) -> Bool {
+	@discardableResult func configure(window: ThemeWindow?, with activity: NSUserActivity) -> Bool {
 		guard let bookmarkUUIDString = activity.userInfo?[ownCloudOpenAccountAccountUuidKey] as? String, let bookmarkUUID = UUID(uuidString: bookmarkUUIDString), let bookmark = OCBookmarkManager.shared.bookmark(for: bookmarkUUID), let navigationController = window?.rootViewController as? ThemeNavigationController, let serverListController = navigationController.topViewController as? ServerListTableViewController else {
 			return false
 		}
