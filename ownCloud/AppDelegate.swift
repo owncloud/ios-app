9341d98d88c3df30593114afd4652b4a1407ef56
Matthias Hühne <mhuehne@owncloud.com>
2020-01-09 09:21:43 +0100

    [release/1.2.1] Release 1.2.1 (#593)
    * - ThemeWindow: add tracking of live ThemeWindows (#591)
    
    - AppLockManager: add support for multiple UIWindowScenes
    - fixes iOS 13 lock issue
    
    * new build and version number for bugfix release 1.2.1
    
    * updated changelog for release 1.2.1
    
    * Added more logs for instant photo upload
    
    Co-authored-by: Felix Schwarz <felix.schwarz@iospirit.com>

--- ownCloud/AppDelegate.swift
+++ ownCloud/AppDelegate.swift
@@ -26,7 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var serverListTableViewController: ServerListTableViewController?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
 		var navigationController: UINavigationController?
 
 		// Set up logging (incl. stderr redirection) and log launch time, app version, build number and commit
@@ -37,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 		ThemeStyle.registerDefaultStyles()
 
-		serverListTableViewController = ServerListTableViewController(style: UITableView.Style.plain)
+		serverListTableViewController = ServerListTableViewController(style: .plain)
 
 		navigationController = ThemeNavigationController(rootViewController: serverListTableViewController!)
 
