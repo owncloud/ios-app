6e2a81ed02b28e05edcb6534765abe8c89a409ed
Matthias Hühne <github@hosy.de>
2020-03-05 15:29:50 +0100

    - fixed title color in release notes - fixed header/footer height, if nil (seen in release notes view controller)

--- ownCloud/Release Notes/ReleaseNotesHostViewController.swift
+++ ownCloud/Release Notes/ReleaseNotesHostViewController.swift
@@ -143,7 +143,7 @@ extension ReleaseNotesHostViewController : Themeable {
 	func applyThemeCollection(theme: Theme, collection: ThemeCollection, event: ThemeEvent) {
 
 		self.view.backgroundColor = collection.tableBackgroundColor
-		titleLabel.applyThemeCollection(collection, itemStyle: .logo)
+		titleLabel.applyThemeCollection(collection, itemStyle: .title)
 		proceedButton.backgroundColor = collection.neutralColors.normal.background
 		proceedButton.setTitleColor(collection.neutralColors.normal.foreground, for: .normal)
 		footerButton.setTitleColor(collection.tableRowColors.labelColor, for: .normal)
