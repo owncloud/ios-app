43c860127d68d9b553b98c68082cb32da3281a4a
Matthias Hühne <github@hosy.de>
2020-03-12 13:27:39 +0100

    - added directory picker to select an import directory - import files from extension context - correct handling cancel on directory picker - load needed classes and images

--- ownCloud Share Extension/ShareViewController.swift
+++ ownCloud Share Extension/ShareViewController.swift
@@ -19,14 +19,35 @@
 import UIKit
 import ownCloudSDK
 import ownCloudAppShared
+import MobileCoreServices
 
 class ShareViewController: MoreStaticTableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		// Implement App Lock
+		//AppLockManager.shared.showLockscreenIfNeeded()
+
+		OCExtensionManager.shared.addExtension(CreateFolderAction.actionExtension)
+		Theme.shared.add(tvgResourceFor: "owncloud-logo")
 		OCItem.registerIcons()
 		setupNavigationBar()
+		setupAccountSelection()
+	}
 
+	@objc private func cancelAction () {
+		let error = NSError(domain: "ShareViewErrorDomain", code: 0, userInfo: [NSLocalizedDescriptionKey: "Canceled by user"])
+		extensionContext?.cancelRequest(withError: error)
+	}
+
+	private func setupNavigationBar() {
+		self.navigationItem.title = OCAppIdentity.shared.appName ?? "ownCloud"
+
+		let itemCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelAction))
+		self.navigationItem.setRightBarButton(itemCancel, animated: false)
+	}
+
+	func setupAccountSelection() {
 		let title = NSAttributedString(string: "Save File".localized, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: .heavy)])
 
 		var actionsRows: [StaticTableViewRow] = []
@@ -37,12 +58,7 @@ class ShareViewController: MoreStaticTableViewController {
 
 		for (bookmark) in bookmarks {
 			let row = StaticTableViewRow(buttonWithAction: { (_ row, _ sender) in
-
 				self.openDirectoryPicker(for: bookmark)
-				/*
-				moreViewController.dismiss(animated: true, completion: {
-				self.importItemWithDirectoryPicker(with: url, into: bookmark)
-				})*/
 			}, title: bookmark.shortName, style: .plain, image: Theme.shared.image(for: "owncloud-logo", size: CGSize(width: 25, height: 25)), imageWidth: 25, alignment: .left)
 			actionsRows.append(row)
 		}
@@ -56,10 +72,7 @@ class ShareViewController: MoreStaticTableViewController {
 			if let core = core, error == nil {
 				let directoryPickerViewController = ClientDirectoryPickerViewController(core: core, path: "/", selectButtonTitle: "Save here".localized, avoidConflictsWith: [], choiceHandler: { (selectedDirectory) in
 					if let targetDirectory = selectedDirectory {
-						print("--> targetDire \(targetDirectory)")
-
-						self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
-						//self.importFile(url: url, to: targetDirectory, bookmark: bookmark, core: core)
+						self.importFiles(to: targetDirectory, bookmark: bookmark, core: core)
 					}
 				})
 				OnMainThread {
@@ -69,22 +82,67 @@ class ShareViewController: MoreStaticTableViewController {
 		})
 	}
 
-	private func setupNavigationBar() {
-		self.navigationItem.title = "ownCloud"
+    func importFiles(to targetDirectory : OCItem, bookmark: OCBookmark, core : OCCore?) {
+        if let inputItems : [NSExtensionItem] = self.extensionContext?.inputItems as? [NSExtensionItem] {
+			print("--> inputItems \(inputItems)")
+            for item : NSExtensionItem in inputItems {
+				if let attachments = item.attachments {
 
-		let itemCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelAction))
-		self.navigationItem.setLeftBarButton(itemCancel, animated: false)
+                    if attachments.isEmpty {
+                        self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+                        return
+                    }
 
-		let itemDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
-		self.navigationItem.setRightBarButton(itemDone, animated: false)
-	}
+                    for current in attachments {
+						print("--> \(current.registeredTypeIdentifiers)")
+                        if current.hasItemConformingToTypeIdentifier(kUTTypeItem as String) {
+                            current.loadItem(forTypeIdentifier: kUTTypeItem as String, options: nil, completionHandler: {(item, error) -> Void in
+								print("..>> item \(item)")
+                                if error == nil {
+                                    if let url = item as? URL {
+                                        print("item as url: \(url)")
+										self.importFile(url: url, to: targetDirectory, bookmark: bookmark, core: core)
+                                    }
+                                } else {
+                                    print("ERROR: \(error)")
+                                }
+                            })
 
-	@objc private func cancelAction () {
-		let error = NSError(domain: "some.bundle.identifier", code: 0, userInfo: [NSLocalizedDescriptionKey: "An error description"])
-		extensionContext?.cancelRequest(withError: error)
-	}
+                        }
+                    }
+                }
+            }
+        }
+		self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+    }
 
-	@objc private func doneAction() {
-		extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+	func importFile(url importItemURL: URL, to targetDirectory : OCItem, bookmark: OCBookmark, core : OCCore?) {
+		let name = importItemURL.lastPathComponent
+		if core?.importItemNamed(name,
+					 at: targetDirectory,
+					 from: importItemURL,
+					 isSecurityScoped: false,
+					 options: [OCCoreOption.importByCopying : true,
+						   OCCoreOption.automaticConflictResolutionNameStyle : OCCoreDuplicateNameStyle.bracketed.rawValue],
+					 placeholderCompletionHandler: { (error, item) in
+						if error != nil {
+							Log.debug("Error uploading \(Log.mask(name)) to \(Log.mask(targetDirectory.path)), error: \(error?.localizedDescription ?? "" )")
+						}
+
+						OnBackgroundQueue(after: 2) {
+							// Return OCCore after 2 seconds, giving the core a chance to schedule the upload with a NSURLSession
+							OCCoreManager.shared.returnCore(for: bookmark, completionHandler: nil)
+						}
+					 },
+					 resultHandler: { (error, _ core, _ item, _) in
+						if error != nil {
+							Log.debug("Error uploading \(Log.mask(name)) to \(Log.mask(targetDirectory.path)), error: \(error?.localizedDescription ?? "" )")
+						} else {
+							Log.debug("Success uploading \(Log.mask(name)) to \(Log.mask(targetDirectory.path))")
+						}
+					}
+		) == nil {
+			Log.debug("Error setting up upload of \(Log.mask(name)) to \(Log.mask(targetDirectory.path))")
+		}
 	}
 }
