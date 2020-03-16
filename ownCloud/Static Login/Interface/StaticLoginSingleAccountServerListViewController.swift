83de4484c44e5edfaf98ed607b720c28e2c241ce
Felix Schwarz <felix.schwarz@iospirit.com>
2020-03-16 10:12:21 +0100

    - Use enums instead of plain numbers for sections and rows

--- ownCloud/Static Login/Interface/StaticLoginSingleAccountServerListViewController.swift
+++ ownCloud/Static Login/Interface/StaticLoginSingleAccountServerListViewController.swift
@@ -20,6 +20,29 @@ import UIKit
 import ownCloudSDK
 
 class StaticLoginSingleAccountServerListViewController: ServerListTableViewController {
+	// Sections in the table view controller
+	private enum SingleAccountSection : Int, CaseIterable {
+		case accessFiles
+		case actions
+		case settings
+	}
+
+	// Rows by section
+	private enum AccessFilesRowIndex : Int, CaseIterable {
+		case accessFiles
+	}
+
+	private enum ActionRowIndex : Int, CaseIterable {
+		case editLogin
+		case manageStorage
+		case logout
+	}
+
+	private enum SettingsRowIndex : Int, CaseIterable {
+		case settings
+	}
+
+	// Implementation
 	var headerView : UIView?
 	weak var staticLoginViewController : StaticLoginViewController?
 
@@ -32,97 +55,113 @@ class StaticLoginSingleAccountServerListViewController: ServerListTableViewContr
 	}
 
 	override func numberOfSections(in tableView: UITableView) -> Int {
-		return 3
+		return SingleAccountSection.allCases.count
 	}
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		switch SingleAccountSection(rawValue: section) {
+			case .accessFiles: return AccessFilesRowIndex.allCases.count
+			case .actions: 	   return ActionRowIndex.allCases.count
+			case .settings:    return SettingsRowIndex.allCases.count
 
-		if section == 0 {
-			return 1
-		} else if section == 1 {
-			return 3
+			default: 	   return 0
 		}
-
-		return 1
 	}
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let section = SingleAccountSection(rawValue: indexPath.section)
+		var rowCell : UITableViewCell
 
-		if indexPath.section == 0 {
-			guard let bookmarkCell = self.tableView.dequeueReusableCell(withIdentifier: "login-cell", for: indexPath) as? ThemeTableViewCell else {
-				return ThemeTableViewCell()
-			}
+		switch section {
+			case .accessFiles:
+				guard let bookmarkCell = self.tableView.dequeueReusableCell(withIdentifier: "login-cell", for: indexPath) as? ThemeTableViewCell else {
+					return ThemeTableViewCell()
+				}
 
-			bookmarkCell.textLabel?.text = "Access Files".localized
-			bookmarkCell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
-			bookmarkCell.layer.cornerRadius = 8
-			bookmarkCell.layer.masksToBounds = true
-			if #available(iOS 13.0, *) {
-				bookmarkCell.imageView?.image = UIImage(systemName: "folder")
-			} else {
-				bookmarkCell.imageView?.image = UIImage(named: "folder")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
-			}
-
-			return bookmarkCell
-		}
-
-		guard let bookmarkCell = self.tableView.dequeueReusableCell(withIdentifier: "tool-cell", for: indexPath) as? ServerListToolCell else {
-			return ServerListToolCell()
-		}
-
-		if indexPath.section == 1 {
-			switch indexPath.row {
-			case 0:
-				bookmarkCell.textLabel?.text = "Edit Login".localized
-
-				bookmarkCell.clipsToBounds = true
+				bookmarkCell.textLabel?.text = "Access Files".localized
+				bookmarkCell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
 				bookmarkCell.layer.cornerRadius = 8
-				bookmarkCell.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-
+				bookmarkCell.layer.masksToBounds = true
 				if #available(iOS 13.0, *) {
-					bookmarkCell.imageView?.image = UIImage(systemName: "square.and.pencil")
+					bookmarkCell.imageView?.image = UIImage(systemName: "folder")
 				} else {
-					bookmarkCell.imageView?.image = UIImage(named: "square.and.pencil")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
+					bookmarkCell.imageView?.image = UIImage(named: "folder")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
 				}
-			case 1:
-				bookmarkCell.textLabel?.text = "Manage Storage".localized
 
-				if #available(iOS 13.0, *) {
-					bookmarkCell.imageView?.image = UIImage(systemName: "arrow.3.trianglepath")
-				} else {
-					bookmarkCell.imageView?.image = UIImage(named: "arrow.3.trianglepath")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
+				rowCell = bookmarkCell
+
+			case .actions:
+				guard let bookmarkCell = self.tableView.dequeueReusableCell(withIdentifier: "tool-cell", for: indexPath) as? ServerListToolCell else {
+					return ServerListToolCell()
 				}
-			case 2:
-				bookmarkCell.textLabel?.text = "Logout".localized
 
-				bookmarkCell.clipsToBounds = true
+				switch ActionRowIndex(rawValue: indexPath.row) {
+					case .editLogin:
+						bookmarkCell.textLabel?.text = "Edit Login".localized
+
+						bookmarkCell.clipsToBounds = true
+						bookmarkCell.layer.cornerRadius = 8
+						bookmarkCell.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+
+						if #available(iOS 13.0, *) {
+							bookmarkCell.imageView?.image = UIImage(systemName: "square.and.pencil")
+						} else {
+							bookmarkCell.imageView?.image = UIImage(named: "square.and.pencil")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
+						}
+
+					case .manageStorage:
+						bookmarkCell.textLabel?.text = "Manage Storage".localized
+
+						if #available(iOS 13.0, *) {
+							bookmarkCell.imageView?.image = UIImage(systemName: "arrow.3.trianglepath")
+						} else {
+							bookmarkCell.imageView?.image = UIImage(named: "arrow.3.trianglepath")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
+						}
+
+					case .logout:
+						bookmarkCell.textLabel?.text = "Logout".localized
+
+						bookmarkCell.clipsToBounds = true
+						bookmarkCell.layer.cornerRadius = 8
+						bookmarkCell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+
+						if #available(iOS 13.0, *) {
+							bookmarkCell.imageView?.image = UIImage(systemName: "power")
+						} else {
+							bookmarkCell.imageView?.image = UIImage(named: "power")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
+						}
+
+					default:
+						bookmarkCell.textLabel?.text = ""
+				}
+
+				rowCell = bookmarkCell
+
+			case .settings:
+				guard let bookmarkCell = self.tableView.dequeueReusableCell(withIdentifier: "tool-cell", for: indexPath) as? ServerListToolCell else {
+					return ServerListToolCell()
+				}
+
+				bookmarkCell.textLabel?.text = "Settings".localized
 				bookmarkCell.layer.cornerRadius = 8
-				bookmarkCell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-
+				bookmarkCell.layer.masksToBounds = true
 				if #available(iOS 13.0, *) {
-					bookmarkCell.imageView?.image = UIImage(systemName: "power")
+					bookmarkCell.imageView?.image = UIImage(systemName: "gear")
 				} else {
-					bookmarkCell.imageView?.image = UIImage(named: "power")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
+					bookmarkCell.imageView?.image = UIImage(named: "gear")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
 				}
+
+				rowCell = bookmarkCell
+
 			default:
-				bookmarkCell.textLabel?.text = ""
-			}
-		} else {
-			bookmarkCell.textLabel?.text = "Settings".localized
-			bookmarkCell.layer.cornerRadius = 8
-			bookmarkCell.layer.masksToBounds = true
-			if #available(iOS 13.0, *) {
-				bookmarkCell.imageView?.image = UIImage(systemName: "gear")
-			} else {
-				bookmarkCell.imageView?.image = UIImage(named: "gear")?.scaledImageFitting(in: CGSize(width: 28, height: 28))
-			}
+				rowCell = ServerListToolCell()
 		}
 
-		return bookmarkCell
+		return rowCell
 	}
 
 	override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-		if section == 0 {
+		if SingleAccountSection(rawValue: section) == .accessFiles {
 			if headerView == nil, let bookmark : OCBookmark = OCBookmarkManager.shared.bookmarks.first, let userName = bookmark.userName {
 				let headerText = String(format: "You are connected as\n%@ to %@".localized, userName, bookmark.shortName)
 				headerView = StaticTableViewSection.buildHeader(title: headerText)
@@ -135,25 +174,34 @@ class StaticLoginSingleAccountServerListViewController: ServerListTableViewContr
 	}
 
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		if indexPath.section == 0 {
-			super.tableView(tableView, didSelectRowAt: indexPath)
-		} else if indexPath.section == 1, let bookmark : OCBookmark = OCBookmarkManager.shared.bookmarks.first {
-			tableView.deselectRow(at: indexPath, animated: true)
-			switch indexPath.row {
-			case 0:
-				showBookmarkUI(edit: bookmark)
-			case 1:
-				showBookmarkInfoUI(bookmark)
-			case 2:
-				delete(bookmark: bookmark, at: IndexPath(row: 0, section: 0) ) {
-					self.staticLoginViewController?.showFirstScreen()
+		switch SingleAccountSection(rawValue: indexPath.section) {
+			case .accessFiles:
+				super.tableView(tableView, didSelectRowAt: indexPath)
+
+			case .actions:
+				if let bookmark : OCBookmark = OCBookmarkManager.shared.bookmarks.first {
+					tableView.deselectRow(at: indexPath, animated: true)
+					switch ActionRowIndex(rawValue: indexPath.row) {
+						case .editLogin:
+							showBookmarkUI(edit: bookmark)
+
+						case .manageStorage:
+							showBookmarkInfoUI(bookmark)
+
+						case .logout:
+							delete(bookmark: bookmark, at: IndexPath(row: 0, section: 0) ) {
+								self.staticLoginViewController?.showFirstScreen()
+							}
+
+						default: break
+					}
 				}
-			default:
-				break
-			}
-		} else {
-			tableView.deselectRow(at: indexPath, animated: true)
-			settings()
+
+			case .settings:
+				tableView.deselectRow(at: indexPath, animated: true)
+				settings()
+
+			default: break
 		}
 	}
 
