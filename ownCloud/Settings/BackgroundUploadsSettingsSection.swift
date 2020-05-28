5ac88f66eeb182698a42854bcad8ff448d00dfe1
Michael Neuwert <mn@neuwert-media.com>
2020-05-28 11:19:22 +0200

    Configured main thread checker not to stop on first hit

---
+++ ownCloud/Settings/BackgroundUploadsSettingsSection.swift
@@ -0,0 +1,9 @@
+//
+//  BackgroundUploadsSettingsSection.swift
+//  ownCloud
+//
+//  Created by Michael Neuwert on 27.05.20.
+//  Copyright © 2020 ownCloud GmbH. All rights reserved.
+//
+
+import Foundation
