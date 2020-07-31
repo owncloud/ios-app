8cde5fa6882e448bce9e92a3e9adbf266975d0e7
Matthias Hühne <github@hosy.de>
2020-07-31 10:44:18 +0200

    save plain text as RTF file

--- ownCloud Share Extension/ShareViewController.swift
+++ ownCloud Share Extension/ShareViewController.swift
@@ -219,16 +219,55 @@ class ShareViewController: MoreStaticTableViewController {
 					for attachment in attachments {
 						if let type = attachment.registeredTypeIdentifiers.first, attachment.hasItemConformingToTypeIdentifier(kUTTypeItem as String) {
 							dispatchGroup.enter()
+							if type == "public.plain-text" {
+								if let attributedString = item.attributedContentText {
+									do {
+										let rtfData = try attributedString.data(from: .init(location: 0, length: attributedString.length),
+																				documentAttributes: [.documentType: NSAttributedString.DocumentType.rtf])
 
-							attachment.loadFileRepresentation(forTypeIdentifier: type) { [weak core] (url, error) in
-								if error == nil, let url = url {
-									core?.importThroughFileProvider(url: url, to: targetDirectory, bookmark: bookmark, completion: { (error) in
-										Log.error("Error importing item at \(url.absoluteString) through file provider: \(String(describing: error))")
+										let tempFilePath = NSTemporaryDirectory() + (attachment.suggestedName ?? "Text".localized) + ".rtf"
+										FileManager.default.createFile(atPath: tempFilePath, contents:rtfData, attributes:nil)
+
+										core?.importThroughFileProvider(url: URL(fileURLWithPath: tempFilePath), to: targetDirectory, bookmark: bookmark, completion: { (_) in
+											try? FileManager.default.removeItem(atPath: tempFilePath)
+											dispatchGroup.leave()
+										})
+									} catch {
+										print(error)
 										dispatchGroup.leave()
-									})
-								} else {
-									Log.error("Error loading item: \(String(describing: error))")
-									dispatchGroup.leave()
+									}
+								}
+
+								/*
+								attachment.loadItem(forTypeIdentifier: type, options: nil, completionHandler: { [weak core] (item, error) -> Void in
+									if error == nil {
+										if let text = item as? String {
+											let ext = self.utiToFileExtension(type)
+											let tempFilePath = NSTemporaryDirectory() + (attachment.suggestedName ?? "Text".localized) + "." + (ext ?? type)
+											FileManager.default.createFile(atPath: tempFilePath, contents:Data(text.utf8), attributes:nil)
+
+											core?.importThroughFileProvider(url: URL(fileURLWithPath: tempFilePath), to: targetDirectory, bookmark: bookmark, completion: { (_) in
+												try? FileManager.default.removeItem(atPath: tempFilePath)
+												dispatchGroup.leave()
+											})
+										}
+									} else {
+										Log.error("Error loading item: \(String(describing: error))")
+										dispatchGroup.leave()
+									}
+								})
+								*/
+							} else {
+								attachment.loadFileRepresentation(forTypeIdentifier: type) { [weak core] (url, error) in
+									if error == nil, let url = url {
+										core?.importThroughFileProvider(url: url, to: targetDirectory, bookmark: bookmark, completion: { (error) in
+											Log.error("Error importing item at \(url.absoluteString) through file provider: \(String(describing: error))")
+											dispatchGroup.leave()
+										})
+									} else {
+										Log.error("Error loading item: \(String(describing: error))")
+										dispatchGroup.leave()
+									}
 								}
 							}
 						}
