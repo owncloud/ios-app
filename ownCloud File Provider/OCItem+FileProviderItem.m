8122244afcb92ac7a608f7ca4d8f06f92a643a7f
Felix Schwarz <fs-git@iospirit.com>
2020-09-09 22:17:19 +0200

    FileProvider: - MIMEType->UTI conversion change: add URL to comment explaining the solution - move MIMEType->UTI conversion log messages from debug to verbose level

--- ownCloud File Provider/OCItem+FileProviderItem.m
+++ ownCloud File Provider/OCItem+FileProviderItem.m
@@ -174,7 +174,7 @@ static NSMutableDictionary<OCLocalID, NSError *> *sOCItemUploadingErrors;
 		{
 			uti = OCItem.overriddenUTIByMIMEType[self.mimeType];
 
-			OCLogDebug(@"Mapped %@ MIMEType %@ to UTI %@", self.name, self.mimeType, uti);
+			OCLogVerbose(@"Mapped %@ MIMEType %@ to UTI %@", self.name, self.mimeType, uti);
 		}
 	}
 
@@ -187,7 +187,7 @@ static NSMutableDictionary<OCLocalID, NSError *> *sOCItemUploadingErrors;
 		{
 			uti = OCItem.overriddenUTIBySuffix[suffix];
 
-			OCLogDebug(@"Mapped %@ suffix %@ to UTI %@", self.name, suffix, uti);
+			OCLogVerbose(@"Mapped %@ suffix %@ to UTI %@", self.name, suffix, uti);
 		}
 	}
 
@@ -203,15 +203,16 @@ static NSMutableDictionary<OCLocalID, NSError *> *sOCItemUploadingErrors;
 			uti = (__bridge NSString *)kUTTypeData;
 		}
 
-		OCLogDebug(@"Converted %@ MIMEType %@ to UTI %@", self.name, self.mimeType, uti);
+		OCLogVerbose(@"Converted %@ MIMEType %@ to UTI %@", self.name, self.mimeType, uti);
 	}
 
 	// Reject "dyn.*" types
 	if ([uti hasPrefix:@"dyn."])
 	{
-		// Use data instead
+		// Use generic data UTI instead
+		// Rationale: https://github.com/owncloud/ios-app/issues/747#issuecomment-689797261
 		uti = (__bridge NSString *)kUTTypeData;
-		OCLogDebug(@"Rejected dynamic %@ UTI for %@, using %@ instead", self.name, self.mimeType, uti);
+		OCLogVerbose(@"Rejected dynamic %@ UTI for %@, using %@ instead", self.name, self.mimeType, uti);
 	}
 
 	return (uti);
