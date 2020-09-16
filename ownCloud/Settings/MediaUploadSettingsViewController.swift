3364b130229d39323da6b9d604a165775d9253a2
Michael Neuwert <mn@neuwert-media.com>
2020-09-17 00:19:37 +0200

    Corrected review findings
    - Avoiding blocking main thread
    - Observing product license asynchronously
    - Reacting to enterprise account addition / removal by observing corresponding notification

--- ownCloud/Settings/MediaUploadSettingsViewController.swift
+++ ownCloud/Settings/MediaUploadSettingsViewController.swift
@@ -23,47 +23,66 @@ import ownCloudAppShared
 
 class MediaUploadSettingsViewController: StaticTableViewController {
 
+	private var proPhotoSettingsSection: StaticTableViewSection?
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		navigationItem.title = "Media Upload".localized
 
 		if let userDefaults = OCAppIdentity.shared.userDefaults {
+			proPhotoSettingsSection = ProPhotoUploadSettingsSection(userDefaults: userDefaults)
+		}
+
+		if let userDefaults = OCAppIdentity.shared.userDefaults {
 			self.addSection(MediaExportSettingsSection(userDefaults: userDefaults))
-
-			let enterpriseAccountAvailable = OCBookmarkManager.shared.bookmarks.filter({$0.edition == .Enterprise}).count > 0
-
-			if enterpriseAccountAvailable || isProPhotoPackageLicensed() {
-				self.addSection(ProPhotoUploadSettingsSection(userDefaults: userDefaults))
-			}
 
 			if OCBookmarkManager.shared.bookmarks.count > 0 {
 				self.addSection(AutoUploadSettingsSection(userDefaults: userDefaults))
 				// TODO: Re-add this section when we re-gain an ability to run background NSURLSessions
 				//self.addSection(BackgroundUploadsSettingsSection(userDefaults: userDefaults))
 			}
+
+			reconsiderProPhotoSettingsSection()
+		}
+
+		NotificationCenter.default.addObserver(self, selector: #selector(bookmarksChanged), name: Notification.Name.OCBookmarkManagerListChanged, object: nil)
+	}
+
+	private func reconsiderProPhotoSettingsSection() {
+
+		guard let proSettingsSection = proPhotoSettingsSection else { return }
+
+		var addProPhotoSettings = false
+
+		if OCLicenseEnterpriseProvider.numberOfEnterpriseAccounts > 0 {
+			addProPhotoSettings = true
+		}
+
+		let featureAuthStatus = OCLicenseManager.shared.authorizationStatus(forFeature: .photoProFeatures, in: OCLicenseEnvironment())
+
+		switch featureAuthStatus {
+		case .unknown:
+			OCLicenseManager.shared.observeProducts(nil, features: [ .photoProFeatures ], in: OCLicenseEnvironment(), withOwner: self) { (licenseObserver, isInitial, _) in
+				// call code that adds and removes the section depending on authStatus and number of enterprise accounts
+				if isInitial {
+					OCLicenseManager.shared.stop(licenseObserver)
+				}
+				self.reconsiderProPhotoSettingsSection()
+			}
+		case .granted:
+			addProPhotoSettings = true
+		default:
+			break
+		}
+
+		if addProPhotoSettings {
+			self.addSection(proSettingsSection)
+		} else {
+			self.removeSection(proSettingsSection)
 		}
 	}
 
-	private func isProPhotoPackageLicensed() -> Bool {
-
-		let environment = OCLicenseEnvironment()
-
-		// Take a shortcut (ha!) if the authorization status is granted
-		if OCLicenseManager.shared.authorizationStatus(forFeature: .photoProFeatures, in: environment) == .granted {
-			return true
-		}
-
-		// Make sure that pending refreshes have been carried out otherwise, so the result is actually conclusive
-		let waitGroup = DispatchGroup()
-
-		waitGroup.enter()
-
-		OCLicenseManager.shared.perform(afterCurrentlyPendingRefreshes: {
-			waitGroup.leave()
-		})
-
-		_ = waitGroup.wait(timeout: .now() + 3)
-
-		return (OCLicenseManager.shared.authorizationStatus(forFeature: .photoProFeatures, in: environment) == .granted)
+	@objc private func bookmarksChanged() {
+		reconsiderProPhotoSettingsSection()
 	}
 }
