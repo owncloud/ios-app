4d468e68b17b3c6f08441723d5074525013427ed
Felix Schwarz <fs-git@iospirit.com>
2020-11-03 00:41:33 +0100

    - new MetadataDocumentationTests with testUpdateConfigurationJSONFromMetadata test for auto-generation of CONFIGURATION.json - add auto-generated doc/CONFIGURATION.json - fix Xcode 12 build error due to CocoaPods/EarlGrey

---
+++ ownCloudTests/Metadata/MetadataDocumentationTests.swift
@@ -0,0 +1,45 @@
+//
+//  MetadataDocumentationTests.swift
+//  ownCloudTests
+//
+//  Created by Felix Schwarz on 02.11.20.
+//  Copyright © 2020 ownCloud GmbH. All rights reserved.
+//
+
+/*
+ * Copyright (C) 2020, ownCloud GmbH.
+ *
+ * This code is covered by the GNU Public License Version 3.
+ *
+ * For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ * You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ */
+
+import XCTest
+import ownCloudSDK
+import ownCloudAppShared
+
+class MetadataDocumentationTests: XCTestCase {
+	func testUpdateConfigurationJSONFromMetadata() throws {
+	 	let sdkDocsURL = Bundle(for: type(of: self)).url(forResource: "class-settings-sdk", withExtension: nil)
+
+		let docDict = OCClassSettings.shared.documentationDictionary(options: [
+			.onlyJSONTypes : true,
+			.externalDocumentationFolders : [ sdkDocsURL ]
+		])
+
+		guard let jsonData = try? JSONSerialization.data(withJSONObject: docDict, options: [.prettyPrinted, .sortedKeys, .fragmentsAllowed]) else {
+			XCTFail("Failed encoding documentation dictionary as JSON")
+			return
+		}
+
+		if let jsonString = String(data: jsonData, encoding: .utf8) {
+			Log.debug("\(jsonString)")
+
+			if let jsonPath = ProcessInfo.processInfo.environment["OC_SETTINGS_DOC_JSON"] {
+				try? jsonData.write(to: URL(fileURLWithPath: jsonPath), options: .atomicWrite)
+			}
+		}
+	}
+}
