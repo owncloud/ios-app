6551b2bc241755a7e56c16b3ef8d8251bab9145f
Dominik Schmidt <dev@dominik-schmidt.de>
2020-11-19 17:12:01 +0100

    Add possibleValues table to settings description

--- tools/GenerateDocs/generate_docs.sh
+++ tools/GenerateDocs/generate_docs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Update/Install CocoaPods
-pod install
+# # Update/Install CocoaPods
+# pod install
 
-# Run test to update CONFIGURATION.json
-xcodebuild test \
--workspace ../../ownCloud.xcworkspace \
--scheme ownCloud \
--destination 'platform=iOS Simulator,name=iPhone 8,OS=latest' \
--only-testing ownCloudTests/MetadataDocumentationTests/testUpdateConfigurationJSONFromMetadata
+# # Run test to update CONFIGURATION.json
+# xcodebuild test \
+# -workspace ../../ownCloud.xcworkspace \
+# -scheme ownCloud \
+# -destination 'platform=iOS Simulator,name=iPhone 8,OS=latest' \
+# -only-testing ownCloudTests/MetadataDocumentationTests/testUpdateConfigurationJSONFromMetadata
 
 # Run gomplate to generate the adoc
 gomplate -f templates/ios_mdm_tables.adoc.tmpl --context config=../../doc/CONFIGURATION.json -o ../../docs/modules/ROOT/pages/ios_mdm_tables.adoc
