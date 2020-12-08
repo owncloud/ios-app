7ce091d2bb309020d56c5dde1e1120822dd4d281
Michael Neuwert <mn@neuwert-media.com>
2020-12-08 20:51:34 +0100

    Using new Photo picker UI on iOS14

--- ownCloud/Client/Actions/Actions+Extensions/UploadMediaAction.swift
+++ ownCloud/Client/Actions/Actions+Extensions/UploadMediaAction.swift
@@ -20,14 +20,72 @@ import UIKit
 import ownCloudSDK
 import ownCloudAppShared
 import Photos
+import PhotosUI
+
+@available(iOS 14, *)
+class PhotoPickerPresenter: PHPickerViewControllerDelegate {
+
+	typealias AssetSelectionHandler = ([PHAsset]) -> Void
+
+	var completionHandler: AssetSelectionHandler?
+	var parentViewController: UIViewController?
+
+	var pickerViewController: UIViewController {
+		var configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+		configuration.preferredAssetRepresentationMode = .automatic
+		configuration.selectionLimit = 0
+		let pickerViewController = PHPickerViewController(configuration: configuration)
+		pickerViewController.delegate = self
+
+		return pickerViewController
+	}
+
+	func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+
+		picker.dismiss(animated: true)
+
+		OnBackgroundQueue {
+
+			var assets = [PHAsset]()
+
+			// Get asset identifiers
+			var identifiers = [String]()
+			for result in results {
+				if let identifier = result.assetIdentifier {
+					identifiers.append(identifier)
+				}
+			}
+
+			// Fetch corresponding assets
+			let fetchResult = PHAsset.fetchAssets(withLocalIdentifiers: identifiers, options: nil)
+			fetchResult.enumerateObjects({ (asset, _, _) in
+				assets.append(asset)
+			})
+
+			OnMainThread {
+				self.completionHandler?(assets)
+			}
+		}
+	}
+
+	func present(in viewController:UIViewController, with completion:@escaping AssetSelectionHandler) {
+		self.parentViewController = viewController
+		self.completionHandler = completion
+		viewController.present(self.pickerViewController, animated: true)
+	}
+}
 
 class UploadMediaAction: UploadBaseAction {
+
 	override class var identifier : OCExtensionIdentifier? { return OCExtensionIdentifier("com.owncloud.action.uploadphotos") }
 	override class var category : ActionCategory? { return .normal }
 	override class var name : String { return "Upload from your photo library".localized }
 	override class var locations : [OCExtensionLocationIdentifier]? { return [.folderAction, .keyboardShortcut] }
 	override class var keyCommand : String? { return "M" }
 	override class var keyModifierFlags: UIKeyModifierFlags? { return [.command] }
+
+	// We need this to keep PhotoPickerPresenter alive in iOS14. Type is 'Any' since it is iOS14 only and you can't add @available() to stored properties
+	private var picker: Any?
 
 	private struct AssociatedKeys {
 		static var actionKey = "action"
@@ -52,20 +110,32 @@ class UploadMediaAction: UploadBaseAction {
 	}
 
 	private func presentImageGalleryPicker() {
+
+		func addAssetsToQueue(assets:[PHAsset]) {
+			guard let path = self.context.items.first?.path else { return }
+			guard let bookmark = self.core?.bookmark else { return }
+
+			MediaUploadQueue.shared.addUploads(assets, for: bookmark, at: path)
+		}
+
 		if let viewController = self.context.viewController {
-			let photoAlbumViewController = PhotoAlbumTableViewController()
-			photoAlbumViewController.selectionCallback = {(assets) in
-				self.completed()
 
-				guard let path = self.context.items.first?.path else { return }
+			if #available(iOS 14, *) {
+				picker = PhotoPickerPresenter()
+				(picker as? PhotoPickerPresenter)?.present(in: viewController, with: { (assets) in
+					self.completed()
+					addAssetsToQueue(assets: assets)
+				})
+			} else {
+				let photoAlbumViewController = PhotoAlbumTableViewController()
+				photoAlbumViewController.selectionCallback = {(assets) in
+					self.completed()
+					addAssetsToQueue(assets: assets)
+				}
+				let navigationController = ThemeNavigationController(rootViewController: photoAlbumViewController)
 
-				guard let bookmark = self.core?.bookmark else { return }
-
-				MediaUploadQueue.shared.addUploads(assets, for: bookmark, at: path)
+				viewController.present(navigationController, animated: true)
 			}
-			let navigationController = ThemeNavigationController(rootViewController: photoAlbumViewController)
-
-			viewController.present(navigationController, animated: true)
 		} else {
 			self.completed(with: NSError(ocError: .internal))
 		}
