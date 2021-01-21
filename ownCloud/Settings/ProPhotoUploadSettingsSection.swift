df3d1fbb0c567399f48608626656b1ff32d38454
Michael Neuwert <michael.neuwert@googlemail.com>
2021-01-21 09:47:24 +0100

    Video upload improvements (#852)
    * Always going through export session when uploading video
    
    Fixes slow motion video upload and other types of video relying on applying transformation data to originally shot footage
    
    * Added settings option to prefer original videos
    
    * - comment out commit badge (as it fails Bitrise builds)
    
    * Fixed a problem with "Prefer original videos" option interferring with "Prefer RAW photos"
    
    Co-authored-by: Michael Neuwert <mn@neuwert-media.com>
    Co-authored-by: Felix Schwarz <fs-git@iospirit.com>
    Co-authored-by: Michael Neuwert <michael.neuwert@gmail.com>

--- ownCloud/Settings/ProPhotoUploadSettingsSection.swift
+++ ownCloud/Settings/ProPhotoUploadSettingsSection.swift
@@ -58,28 +58,39 @@ extension AVCaptureDevice {
 }
 
 extension UserDefaults {
-	enum ProPhotoUploadSettingsKeys : String {
+	enum ProMediaUploadSettingsKeys : String {
 		case PreferOriginals = "pro-photo-upload-prefer-originals"
 		case PreferRAW = "pro-photo-upload-prefer-raw"
+		case PreferOriginalVideos = "pro-video-upload-prefer-originals"
 	}
 
 	public var preferOriginalPhotos: Bool {
 		set {
-			self.set(newValue, forKey: ProPhotoUploadSettingsKeys.PreferOriginals.rawValue)
+			self.set(newValue, forKey: ProMediaUploadSettingsKeys.PreferOriginals.rawValue)
 		}
 
 		get {
-			return self.bool(forKey: ProPhotoUploadSettingsKeys.PreferOriginals.rawValue)
+			return self.bool(forKey: ProMediaUploadSettingsKeys.PreferOriginals.rawValue)
 		}
 	}
 
 	public var preferRawPhotos: Bool {
 		set {
-			self.set(newValue, forKey: ProPhotoUploadSettingsKeys.PreferRAW.rawValue)
+			self.set(newValue, forKey: ProMediaUploadSettingsKeys.PreferRAW.rawValue)
 		}
 
 		get {
-			return self.bool(forKey: ProPhotoUploadSettingsKeys.PreferRAW.rawValue)
+			return self.bool(forKey: ProMediaUploadSettingsKeys.PreferRAW.rawValue)
+		}
+	}
+
+	public var preferOriginalVideos: Bool {
+		set {
+			self.set(newValue, forKey: ProMediaUploadSettingsKeys.PreferOriginalVideos.rawValue)
+		}
+
+		get {
+			return self.bool(forKey: ProMediaUploadSettingsKeys.PreferOriginalVideos.rawValue)
 		}
 	}
 }
@@ -98,14 +109,20 @@ class ProPhotoUploadSettingsSection: SettingsSection {
 
 		self.add(row: preferOriginalsRow)
 
-		if AVCaptureDevice.rawCameraDeviceAvailable() {
-			let preferRawRow = StaticTableViewRow(switchWithAction: { (_, sender) in
-				if let enableSwitch = sender as? UISwitch {
-					userDefaults.preferRawPhotos = enableSwitch.isOn
-				}
-				}, title: "Prefer RAW photos".localized, value: self.userDefaults.preferRawPhotos, identifier: "prefer-raw")
+        let preferRawRow = StaticTableViewRow(switchWithAction: { (_, sender) in
+            if let enableSwitch = sender as? UISwitch {
+                userDefaults.preferRawPhotos = enableSwitch.isOn
+            }
+            }, title: "Prefer RAW photos".localized, value: self.userDefaults.preferRawPhotos, identifier: "prefer-raw")
 
-			self.add(row: preferRawRow)
-		}
+        self.add(row: preferRawRow)
+
+		let preferOriginalVideosRow = StaticTableViewRow(switchWithAction: { (_, sender) in
+			if let enableSwitch = sender as? UISwitch {
+				userDefaults.preferOriginalVideos = enableSwitch.isOn
+			}
+			}, title: "Prefer original videos".localized, value: self.userDefaults.preferOriginalVideos, identifier: "prefer-original-videos")
+
+		self.add(row: preferOriginalVideosRow)
 	}
 }
