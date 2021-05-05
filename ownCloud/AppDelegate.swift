f4124ed43e22e66241cccb154634ebd62648f252
Felix Schwarz <fs-git@iospirit.com>
2021-05-05 13:34:41 +0200

    - add `oc:` scheme to Info.plist to support routing of browser sessions through external browsers - add support for routing responses to AppDelegate and SceneDelegate - add additional parameters to scheme to test CustomScheme browser session handling - update SDK

--- ownCloud/AppDelegate.swift
+++ ownCloud/AppDelegate.swift
@@ -173,11 +173,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}
 
 	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-		if url.matchesAppScheme {
+		if !OCAuthenticationBrowserSessionCustomScheme.handleOpen(url), // No custom scheme URL handling for this URL
+		   url.matchesAppScheme { // + URL matches app scheme
 			guard let window = UserInterfaceContext.shared.currentWindow else { return false }
 
 			openPrivateLink(url: url, in: window)
-
 		} else {
 			var copyBeforeUsing = true
 			if let shouldOpenInPlace = options[UIApplication.OpenURLOptionsKey.openInPlace] as? Bool {
