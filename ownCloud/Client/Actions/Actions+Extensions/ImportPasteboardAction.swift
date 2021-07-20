1f4fe8fb00eda3ff874ce825e71f7ee81f922510
Matthias Hühne <github@hosy.de>
2021-07-20 16:06:14 +0200

    moved import code for general pasteboard to NSItemProvider.loadFileRepresentation - prevents filenames - handles txt pasteboard

--- ownCloud/Client/Actions/Actions+Extensions/ImportPasteboardAction.swift
+++ ownCloud/Client/Actions/Actions+Extensions/ImportPasteboardAction.swift
@@ -21,6 +21,16 @@ import ownCloudSDK
 import MobileCoreServices
 import ownCloudAppShared
 
+extension String {
+	func trimIllegalCharacters() -> String {
+		return self
+			.trimmingCharacters(in: .illegalCharacters)
+			.trimmingCharacters(in: .whitespaces)
+			.trimmingCharacters(in: .newlines)
+			.filter({ $0.isASCII })
+	}
+}
+
 class ImportPasteboardAction : Action {
 	override class var identifier : OCExtensionIdentifier? { return OCExtensionIdentifier("com.owncloud.action.importpasteboard") }
 	override class var category : ActionCategory? { return .normal }
@@ -83,46 +93,93 @@ class ImportPasteboardAction : Action {
 			}
 		} else {
 			// System-wide Pasteboard
-			for type in generalPasteboard.types {
-				guard let dataArray = generalPasteboard.data(forPasteboardType: type, inItemSet: nil) else { return }
-				for data in dataArray {
 
-					if let extUTI = UTTypeCopyPreferredTagWithClass(type as CFString, kUTTagClassFilenameExtension)?.takeRetainedValue() {
-						let fileName = type
-						let localURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName).appendingPathExtension(extUTI as String)
-						do {
-							try data.write(to: localURL)
+			for item in generalPasteboard.itemProviders {
+				let typeIdentifiers = item.registeredTypeIdentifiers
+				let preferredUTIs = [
+					kUTTypeImage,
+					kUTTypeMovie,
+					kUTTypePDF,
+					kUTTypeText,
+					kUTTypeRTF,
+					kUTTypeHTML,
+					kUTTypePlainText
+				]
+				var useUTI : String?
+				var useIndex : Int = Int.max
 
-							core.importItemNamed(localURL.lastPathComponent,
-												 at: rootItem,
-												 from: localURL,
-												 isSecurityScoped: false,
-												 options: [
-													OCCoreOption.importByCopying : false,
-													OCCoreOption.automaticConflictResolutionNameStyle : OCCoreDuplicateNameStyle.bracketed.rawValue
-								],
-												 placeholderCompletionHandler: { (error, item) in
-													if error != nil {
-														self.completed(with: error)
-														Log.debug("Error uploading \(Log.mask(fileName)) to \(Log.mask(rootItem.path)), error: \(error?.localizedDescription ?? "" )")
-													}
-							},
-												 resultHandler: { (error, _ core, _ item, _) in
-													if error != nil {
-														self.completed(with: error)
-														Log.debug("Error uploading \(Log.mask(fileName)) to \(Log.mask(rootItem.path)), error: \(error?.localizedDescription ?? "" )")
-													} else {
-														Log.debug("Success uploading \(Log.mask(fileName)) to \(Log.mask(rootItem.path))")
-													}
+				for typeIdentifier in typeIdentifiers {
+					if !typeIdentifier.hasPrefix("dyn.") {
+						for preferredUTI in preferredUTIs {
+							let conforms = UTTypeConformsTo(typeIdentifier as CFString, preferredUTI)
+
+							// Log.log("\(preferredUTI) vs \(typeIdentifier) -> \(conforms)")
+
+							if conforms {
+								if let utiIndex = preferredUTIs.index(of: preferredUTI), utiIndex < useIndex {
+									useUTI = typeIdentifier
+									useIndex = utiIndex
+								}
 							}
-							)
-						} catch let error as NSError {
-							self.completed(with: error)
 						}
 					}
 				}
+
+				if useUTI == nil, typeIdentifiers.count == 1 {
+					useUTI = typeIdentifiers.first
+				}
+
+				if useUTI == nil {
+					useUTI = kUTTypeData as String
+				}
+
+				var fileName: String?
+
+				item.loadFileRepresentation(forTypeIdentifier: useUTI!) { (url, _ error) in
+					guard let url = url else { return }
+
+					let fileNameMaxLength = 16
+
+					if useUTI == kUTTypeUTF8PlainText as String {
+						fileName = try? String(String(contentsOf: url, encoding: .utf8).prefix(fileNameMaxLength) + ".txt")
+					}
+
+					if useUTI == kUTTypeRTF as String {
+						let options = [NSAttributedString.DocumentReadingOptionKey.documentType : NSAttributedString.DocumentType.rtf]
+						fileName = try? String(NSAttributedString(url: url, options: options, documentAttributes: nil).string.prefix(fileNameMaxLength) + ".rtf")
+					}
+
+					fileName = fileName?
+						.trimmingCharacters(in: .illegalCharacters)
+						.trimmingCharacters(in: .whitespaces)
+						.trimmingCharacters(in: .newlines)
+						.filter({ $0.isASCII })
+
+					if fileName == nil {
+						fileName = url.lastPathComponent
+					}
+					
+					guard let name = fileName else { return }
+
+					self.upload(itemURL: url, rootItem: rootItem, name: name)
+				}
 			}
 		}
+	}
+
+	open func upload(itemURL: URL, rootItem: OCItem, name: String, completionHandler: ClientActionCompletionHandler? = nil) {
+		core?.importItemNamed(name, at: rootItem, from: itemURL, isSecurityScoped: false, options: [
+			OCCoreOption.importByCopying : false,
+			OCCoreOption.automaticConflictResolutionNameStyle : OCCoreDuplicateNameStyle.bracketed.rawValue
+		], placeholderCompletionHandler: nil, resultHandler: { (error, _ core, _ item, _) in
+			if error != nil {
+				Log.debug("Error uploading \(Log.mask(name)) file to \(Log.mask(rootItem.path))")
+				completionHandler?(false)
+			} else {
+				Log.debug("Success uploading \(Log.mask(name)) file to \(Log.mask(rootItem.path))")
+				completionHandler?(true)
+			}
+		})
 	}
 
 	override class func iconForLocation(_ location: OCExtensionLocationIdentifier) -> UIImage? {
