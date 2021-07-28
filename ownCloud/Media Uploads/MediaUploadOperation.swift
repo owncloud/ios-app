06a0ca0c7b710de2fa841b48b8634e821fec6204
Felix Schwarz <fs-git@iospirit.com>
2021-07-28 22:46:39 +0200

    Ensure that each MediaUploadOperation schedules only one asset import.

--- ownCloud/Media Uploads/MediaUploadOperation.swift
+++ ownCloud/Media Uploads/MediaUploadOperation.swift
@@ -28,6 +28,7 @@ class MediaUploadOperation : Operation {
 	private var mediaUploadJob: MediaUploadJob
 	private var assetId: String
 	private var itemTracking : OCCoreItemTracking?
+	private var didImportAsset : Bool = false
 
 	// Session object to enque uploads in file provider extension
 	private var fpSession: OCFileProviderServiceSession?
@@ -109,19 +110,20 @@ class MediaUploadOperation : Operation {
 				return
 			}
 
-			// Perform asset import
-			if let itemLocalId = self.importAsset(asset: asset,
-												  with: core,
-												  at: item,
-												  uploadCompletion: {
-				// Import successful
-				self.removeUploadJob(with: path)
-			}) {
+			// Ensure the asset of the import is only run once
+			if !self.didImportAsset {
+				self.didImportAsset = true
 
-				// Update media upload storage object
-				core.bookmark.modifyMediaUploadStorage { (storage) in
-					storage.update(localItemID: itemLocalId, assetId: self.assetId, targetPath: path)
-					return storage
+				// Perform asset import
+				if let itemLocalId = self.importAsset(asset: asset, with: core, at: item, uploadCompletion: {
+					// Import successful
+					self.removeUploadJob(with: path)
+				}) {
+					// Update media upload storage object
+					core.bookmark.modifyMediaUploadStorage { (storage) in
+						storage.update(localItemID: itemLocalId, assetId: self.assetId, targetPath: path)
+						return storage
+					}
 				}
 			}
 		})
