58a1acae9716d353bdc4ab228d98f06f5683c809
Matthias Hühne <>
2022-10-26 14:30:28 +0200

    solved another edge case on the iPad if the UI is in compact view (can be tested in split view)

--- ownCloud/Client/Actions/EditDocumentViewController.swift
+++ ownCloud/Client/Actions/EditDocumentViewController.swift
@@ -124,25 +124,22 @@ class EditDocumentViewController: QLPreviewController, Themeable {
 	}
 
 	@objc func enableEditingMode() {
-		// Activate editing mode by performing the action on pencil icon. Unfortunately that's the only way to do it apparently
+        // Activate editing mode by performing the action on pencil icon. Unfortunately that's the only way to do it apparently
         if #available(iOS 16.0, *) {
-            if UIDevice().isIpad {
-                if self.navigationItem.rightBarButtonItems?.count ?? 0 > 0 {
-                    guard let markupButton = self.navigationItem.rightBarButtonItems?[0] else { return }
-                    _ = markupButton.target?.perform(markupButton.action, with: markupButton)
-                }
-            } else {
-                if self.toolbarItems?.count ?? 0 > 4 {
-                    guard let markupButton = self.toolbarItems?[4] else { return }
-                    _ = markupButton.target?.perform(markupButton.action, with: markupButton)
-                } else if self.toolbarItems?.count ?? 0 > 1, self.toolbarItems?.count ?? 0 < 4 {
-                    guard let markupButton = self.toolbarItems?[2] else { return }
-                    _ = markupButton.target?.perform(markupButton.action, with: markupButton)
-                }
+            if self.navigationItem.rightBarButtonItems?.count ?? 0 > 0 {
+                guard let markupButton = self.navigationItem.rightBarButtonItems?[0] else { return }
+                _ = markupButton.target?.perform(markupButton.action, with: markupButton)
+            }
+            else if self.toolbarItems?.count ?? 0 > 4 {
+                guard let markupButton = self.toolbarItems?[4] else { return }
+                _ = markupButton.target?.perform(markupButton.action, with: markupButton)
+            } else if self.toolbarItems?.count ?? 0 > 1, self.toolbarItems?.count ?? 0 < 4 {
+                guard let markupButton = self.toolbarItems?[2] else { return }
+                _ = markupButton.target?.perform(markupButton.action, with: markupButton)
             }
         } else if #available(iOS 15.0, *) {
-			if self.navigationItem.rightBarButtonItems?.count ?? 0 > 2 {
-				guard let markupButton = self.navigationItem.rightBarButtonItems?[1] else { return }
+            if self.navigationItem.rightBarButtonItems?.count ?? 0 > 2 {
+                guard let markupButton = self.navigationItem.rightBarButtonItems?[1] else { return }
 				_ = markupButton.target?.perform(markupButton.action, with: markupButton)
 			} else if UIDevice.current.isIpad, self.navigationItem.rightBarButtonItems?.count ?? 0 == 2 {
 				guard let markupButton = self.navigationItem.rightBarButtonItems?[1] else { return }
