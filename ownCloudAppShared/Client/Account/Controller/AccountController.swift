915499eb18b9bf3e7585f8b279465dff979b64e8
Matthias Hühne <github@hosy.de>
2023-06-22 11:06:29 +0200

    implemented recents item in Quick Access section

--- ownCloudAppShared/Client/Account/Controller/AccountController.swift
+++ ownCloudAppShared/Client/Account/Controller/AccountController.swift
@@ -89,6 +89,7 @@ public class AccountController: NSObject, OCDataItem, OCDataItemVersioning, Acco
 			case searchImages
 			case searchVideos
 			case searchAudios
+			case recents
 
 		case activity
 	}
@@ -403,7 +404,16 @@ public class AccountController: NSObject, OCDataItem, OCDataItemVersioning, Acco
 			// Quick access
 			if configuration.showQuickAccess {
 				var quickAccessItems: [OCDataItem & OCDataItemVersioning] = []
-
+				
+				// Recents
+				if specialItems[.recents] == nil {
+					specialItems[.recents] = OCSavedSearch(scope: .account, location: nil, name: "Recents".localized, isTemplate: false, searchTerm: ":1w :file").withCustomIcon(name: "clock.arrow.circlepath").useNameAsTitle(true)
+					
+					if let sideBarItem = specialItems[.recents] {
+						quickAccessItems.append(sideBarItem)
+					}
+				}
+				
 				// Favorites
 				if bookmark?.hasCapability(.favorites) == true {
 					if specialItems[.favoriteItems] == nil {
