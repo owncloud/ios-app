2a4f05cd17c7862c1181361f7842256440a43068
Matthias Hühne <github@hosy.de>
2023-10-12 14:26:49 +0200

    check if script is executed in the correct path

--- ownCloud/Resources/Theming/generate.sh
+++ ownCloud/Resources/Theming/generate.sh
@@ -8,17 +8,21 @@ current_dir=$(pwd)
 theming_dir="/ownCloud/Resources/Theming"
 theme=$1
 
-git submodule init
-git submodule update
+if [ -d "$theming_dir" ]; then
+	git submodule init
+	git submodule update
 
-rename 's/current./theme./' $current_dir$theming_dir/*.*
-mv $current_dir$theming_dir/theme.$theme $current_dir$theming_dir/current.$theme
+	rename 's/current./theme./' $current_dir$theming_dir/*.*
+	mv $current_dir$theming_dir/theme.$theme $current_dir$theming_dir/current.$theme
 
-cp $current_dir$theming_dir/current*/*.png $current_dir$theming_dir/
-cp $current_dir$theming_dir/current*/*.json $current_dir$theming_dir/Branding.json
+	cp $current_dir$theming_dir/current*/*.png $current_dir$theming_dir/
+	cp $current_dir$theming_dir/current*/*.json $current_dir$theming_dir/Branding.json
 
-fastlane generate_appicon
+	fastlane generate_appicon
 
-gomplate --file ./tools/gomplate/Branding.plist.tmpl \
---context config=$current_dir$theming_dir/Branding.json \
---out $current_dir$theming_dir/Branding.plist
+	gomplate --file ./tools/gomplate/Branding.plist.tmpl \
+	--context config=$current_dir$theming_dir/Branding.json \
+	--out $current_dir$theming_dir/Branding.plist
+else
+    echo "Directory $theming_dir does not exist. Maybe this script is executed in the wrong path."
+fi
