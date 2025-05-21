ef4d2e3fc7b0e9028832ac33a79dc41546572848
Felix Schwarz <fs-git@iospirit.com>
2025-05-21 12:10:55 +0200

    - RecentLocation, RecentLocationStore: 	- add new type to store recently picked locations 	- add new helper class utilizing an accounts KVS to locally store and retrieve recently used locations - ClientLocationPicker: add preliminary support for recent locations

---
+++ ownCloudAppShared/Client/View Controllers/Location Picker/Recent Location/RecentLocation.swift
@@ -0,0 +1,60 @@
+//
+//  RecentLocation.swift
+//  ownCloudAppShared
+//
+//  Created by Felix Schwarz on 20.05.25.
+//  Copyright © 2025 ownCloud GmbH. All rights reserved.
+//
+
+/*
+ * Copyright (C) 2025, ownCloud GmbH.
+ *
+ * This code is covered by the GNU Public License Version 3.
+ *
+ * For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ * You should have received a copy of this license along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ */
+
+import UIKit
+import ownCloudSDK
+
+@objc class RecentLocation: NSObject, NSSecureCoding, OCDataItem, OCDataItemVersioning {
+	var location: OCLocation?
+	var timestamp: Date?
+
+	init(location: OCLocation, timestamp: Date = .now) {
+		self.location = location
+		self.timestamp = timestamp
+	}
+
+	// MARK: - NSSecureCoding
+	static var supportsSecureCoding: Bool = true
+
+	func encode(with coder: NSCoder) {
+		coder.encode(location, forKey: "location")
+		coder.encode(timestamp, forKey: "timestamp")
+	}
+
+	required init?(coder: NSCoder) {
+		location = coder.decodeObject(of: OCLocation.self, forKey: "location")
+		timestamp = coder.decodeObject(of: NSDate.self, forKey: "timestamp") as? Date
+	}
+
+	// MARK: - OCDataItem + Versioning
+	var dataItemType: OCDataItemType {
+		return .recentLocation
+	}
+
+	var dataItemReference: OCDataItemReference {
+		return location?.dataItemReference ?? ("\(self)" as NSString)
+	}
+
+	var dataItemVersion: OCDataItemVersion {
+		return "\(location?.dataItemVersion ?? ("-" as NSString))@\(timestamp?.timeIntervalSince1970 ?? 0)" as NSString
+	}
+}
+
+extension OCDataItemType {
+	static let recentLocation: OCDataItemType = OCDataItemType(rawValue: "recentLocation")
+}
